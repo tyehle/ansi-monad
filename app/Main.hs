4ad408fbd06b52432fa379a067a8521e2232e111
Tobin Yehle <tobinyehle@gmail.com>
2019-07-20 22:21:57 -0700

    SGR state and BFS planning

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,185 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
 
-import Lib
+-- import Control.Monad.State
+-- import Data.List (intercalate)
+-- import Data.Word (Word8)
 
 main :: IO ()
-main = someFunc
+main = putStrLn "Hi"
+
+-- data CSICode = ED Int
+--              | SGR SGRCode
+--              deriving Show
+
+-- data SGRCode = Reset
+--              | SetBold | SetFaint | UnsetBoldFaint
+--              | SetItalic | UnsetItalic
+--              | SetUnderline | UnsetUnderline
+--              | SetFGBasic BasicColor | SetBGBasic BasicColor
+--              | SetFG8 Word8 | SetBG8 Word8
+--              | SetFG24 Word8 Word8 Word8
+--              | SetBG24 Word8 Word8 Word8
+--              deriving Show
+
+-- data Color = Color4 BasicColor
+--            | Color8 Word8
+--            | Color24 Word8 Word8 Word8
+--            deriving Show
+
+-- data BasicColor = Black
+--                 | Red | Green | Blue
+--                 | Yellow | Cyan | Magenta
+--                 | White
+--                 | BrightBlack
+--                 | BrightRed | BrightGreen | BrightBlue
+--                 | BrightYellow | BrightCyan | BrightMagenta
+--                 | BrightWhite
+--                 deriving Show
+
+-- data AnsiState = AnsiState
+--   { partial :: String
+--   , bold :: Bool
+--   , faint :: Bool
+--   , italic :: Bool
+--   , underline :: Bool
+--   , fg :: Maybe Color
+--   , bg :: Maybe Color
+--   }
+
+
+-- appendString :: String -> AnsiState -> AnsiState
+-- appendString text s@AnsiState{partial=prev} =
+--   s{ partial = prev ++ text }
+
+
+-- applyCode :: CSICode -> AnsiState -> AnsiState
+-- applyCode code state = updateState code withText
+--   where
+--     withText = appendString (serialize code) state
+
+--     updateState :: CSICode -> AnsiState -> AnsiState
+--     updateState (ED _) _ = undefined
+--     updateState (SGR Reset) s = s{fg=Nothing, bg=Nothing}
+--     updateState (SGR SetBold) s = s{bold=True}
+--     updateState (SGR SetFaint) s = s{faint=True}
+--     updateState (SGR UnsetBoldFaint) s = s{faint=False, bold=False}
+--     updateState (SGR SetItalic) s = s{italic=True}
+--     updateState (SGR UnsetItalic) s = s{italic=False}
+--     updateState (SGR SetUnderline) s = s{underline=True}
+--     updateState (SGR UnsetUnderline) s = s{underline=False}
+--     updateState (SGR (SetFGBasic c)) s = s{fg=Just (Color4 c)}
+--     updateState (SGR (SetFG8 c)) s = s{fg=Just (Color8 c)}
+--     updateState (SGR (SetFG24 r g b)) s = s{fg=Just (Color24 r g b)}
+--     updateState (SGR (SetBGBasic c)) s = s{bg=Just (Color4 c)}
+--     updateState (SGR (SetBG8 c)) s = s{bg=Just (Color8 c)}
+--     updateState (SGR (SetBG24 r g b)) s = s{bg=Just (Color24 r g b)}
+
+
+-- initialAnsiState :: AnsiState
+-- initialAnsiState = AnsiState
+--   { partial = ""
+--   , bold = False
+--   , faint = False
+--   , italic = False
+--   , underline = False
+--   , fg = Nothing
+--   , bg = Nothing
+--   }
+
+
+-- newtype AnsiBuilder a = AnsiBuilder (State AnsiState a)
+--   deriving (Functor, Applicative, Monad)
+
+
+-- colorOffset :: BasicColor -> Int
+-- colorOffset Black   = 0
+-- colorOffset Red     = 1
+-- colorOffset Green   = 2
+-- colorOffset Yellow  = 3
+-- colorOffset Blue    = 4
+-- colorOffset Magenta = 5
+-- colorOffset Cyan    = 6
+-- colorOffset White   = 7
+-- colorOffset BrightBlack   = 60
+-- colorOffset BrightRed     = 61
+-- colorOffset BrightGreen   = 62
+-- colorOffset BrightYellow  = 63
+-- colorOffset BrightBlue    = 64
+-- colorOffset BrightMagenta = 65
+-- colorOffset BrightCyan    = 66
+-- colorOffset BrightWhite   = 67
+
+
+-- serialize :: CSICode -> String
+-- serialize (ED _) = undefined
+-- serialize (SGR Reset) = "\ESC[m"
+-- serialize (SGR SetBold) = "\ESC[1m"
+-- serialize (SGR SetFaint) = "\ESC[2m"
+-- serialize (SGR UnsetBoldFaint) = "\ESC[22m"
+-- serialize (SGR SetItalic) = "\ESC[3m"
+-- serialize (SGR UnsetItalic) = "\ESC[23m"
+-- serialize (SGR SetUnderline) = "\ESC[4m"
+-- serialize (SGR UnsetUnderline) = "\ESC[24m"
+-- serialize (SGR (SetFGBasic c)) = "\ESC[" ++ show n ++ "m"
+--   where n = 30 + colorOffset c
+-- serialize (SGR (SetFG8 c)) = "\ESC[38;5;" ++ show c ++ "m"
+-- serialize (SGR (SetFG24 r g b)) = "\ESC[38;2;" ++ code ++ "m"
+--   where code = intercalate ";" $ map show [r,g,b]
+-- serialize (SGR (SetBGBasic c)) = "\ESC[" ++ show n ++ "m"
+--   where n = 40 + colorOffset c
+-- serialize (SGR (SetBG8 c)) = "\ESC[48;5;" ++ show c ++ "m"
+-- serialize (SGR (SetBG24 r g b)) = "\ESC[48;2;" ++ code ++ "m"
+--   where code = intercalate ";" $ map show [r,g,b]
+
+
+-- ansiPrint :: String -> AnsiBuilder ()
+-- ansiPrint text = AnsiBuilder $ modify (appendString text)
+
+-- ansiPrintLn :: String -> AnsiBuilder ()
+-- ansiPrintLn text = AnsiBuilder $ modify (appendString $ text++"\n")
+
+
+-- ansiCode :: CSICode -> AnsiBuilder ()
+-- ansiCode = AnsiBuilder . modify . applyCode
+
+
+-- reset :: AnsiBuilder ()
+-- reset = ansiCode $ SGR Reset
+
+
+-- setFG :: BasicColor -> AnsiBuilder ()
+-- setFG = setFGColor . Color4
+
+-- setFG8 :: Word8 -> AnsiBuilder ()
+-- setFG8 = setFGColor . Color8
+
+-- setFGrgb :: Word8 -> Word8 -> Word8 -> AnsiBuilder ()
+-- setFGrgb r g b = setFGColor $ Color24 r g b
+
+-- setFGColor :: Color -> AnsiBuilder ()
+-- setFGColor (Color4 color) = ansiCode $ SGR (SetFGBasic color)
+-- setFGColor (Color8 code) = ansiCode $ SGR (SetFG8 code)
+-- setFGColor (Color24 r g b) = ansiCode $ SGR (SetFG24 r g b)
+
+
+-- setBG :: BasicColor -> AnsiBuilder ()
+-- setBG = setBGColor . Color4
+
+-- setBG8 :: Word8 -> AnsiBuilder ()
+-- setBG8 = setBGColor . Color8
+
+-- setBGrgb :: Word8 -> Word8 -> Word8 -> AnsiBuilder ()
+-- setBGrgb r g b = setBGColor $ Color24 r g b
+
+-- setBGColor :: Color -> AnsiBuilder ()
+-- setBGColor (Color4 color) = ansiCode $ SGR (SetBGBasic color)
+-- setBGColor (Color8 code) = ansiCode $ SGR (SetBG8 code)
+-- setBGColor (Color24 r g b) = ansiCode $ SGR (SetBG24 r g b)
+
+
+-- execAnsiBuilder :: AnsiBuilder a -> String
+-- execAnsiBuilder (AnsiBuilder state) = partial finalState
+--   where
+--     finalState = execState state initialAnsiState
---
+++ src/System/Console/ANSI/Monad.hs
@@ -0,0 +1,138 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module System.Console.ANSI.Monad where
+
+import Data.Colour (Colour)
+import Data.Colour.SRGB (toSRGB24, RGB(..))
+import Data.Foldable (foldl')
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (catMaybes)
+import Data.Ord (comparing)
+import Data.Sequence (Seq, Seq(..), (><))
+import qualified Data.Sequence as Seq
+import Data.Word (Word8)
+import System.Console.ANSI.Types
+
+
+data SGRState = SGRState
+  { bold :: Bool
+  , faint :: Bool
+  , italicized :: Bool
+  , underlining :: Underlining
+  , blinkSpeed :: BlinkSpeed
+  , visible :: Bool
+  , inverted :: Bool
+  , foreground :: Maybe ANSIColor
+  , background :: Maybe ANSIColor
+  }
+  deriving (Show, Read, Eq, Ord)
+
+
+data ANSIColor = Color ColorIntensity Color
+               | RGBColor (Colour Float)
+               | PaletteColor Word8
+               deriving (Show, Read, Eq)
+
+instance Ord ANSIColor where
+  compare (Color i1 c1) (Color i2 c2) = compare (i1, c1) (i2, c2)
+  compare (Color _ _) _ = LT
+  compare _ (Color _ _) = GT
+  compare (RGBColor c1) (RGBColor c2) = comparing ((\(RGB r g b) -> (r, g, b)) . toSRGB24) c1 c2
+  compare (RGBColor _) _ = LT
+  compare _ (RGBColor _) = GT
+  compare (PaletteColor w1) (PaletteColor w2) = compare w1 w2
+
+
+defaultState :: SGRState
+defaultState = SGRState
+  { bold = False
+  , faint = False
+  , italicized = False
+  , underlining = NoUnderline
+  , blinkSpeed = SlowBlink
+  , visible = True
+  , inverted = False
+  , foreground = Nothing
+  , background = Nothing
+  }
+
+
+applySGR :: SGR -> SGRState -> SGRState
+applySGR Reset _ = defaultState
+applySGR (SetConsoleIntensity BoldIntensity) s = s{bold=True}
+applySGR (SetConsoleIntensity FaintIntensity) s = s{faint=True}
+applySGR (SetConsoleIntensity NormalIntensity) s = s{bold=False, faint=False}
+applySGR (SetItalicized i) s = s{italicized=i}
+applySGR (SetUnderlining u) s = s{underlining=u}
+applySGR (SetBlinkSpeed b) s = s{blinkSpeed=b}
+applySGR (SetVisible v) s = s{visible=v}
+applySGR (SetSwapForegroundBackground i) s = s{inverted=i}
+applySGR (SetColor Foreground i c) s = s{foreground=Just $ Color i c}
+applySGR (SetColor Background i c) s = s{background=Just $ Color i c}
+applySGR (SetPaletteColor Foreground i) s = s{foreground=Just $ PaletteColor i}
+applySGR (SetPaletteColor Background i) s = s{background=Just $ PaletteColor i}
+applySGR (SetRGBColor Foreground c) s = s{foreground=Just $ RGBColor c}
+applySGR (SetRGBColor Background c) s = s{background=Just $ RGBColor c}
+
+
+allCodes :: [ANSIColor] -> [SGR]
+allCodes colors = enumerableCodes ++ colorCodes
+  where
+    colorCode :: ConsoleLayer -> ANSIColor -> SGR
+    colorCode l (Color i c) = SetColor l i c
+    colorCode l (PaletteColor c) = SetPaletteColor l c
+    colorCode l (RGBColor c) = SetRGBColor l c
+
+    colorCodes = [colorCode l c | l <- [Foreground, Background], c <- colors]
+
+    enumerableCodes = concat
+      [ [Reset]
+      , [SetConsoleIntensity i | i <- [BoldIntensity, FaintIntensity, NormalIntensity]]
+      , [SetItalicized i | i <- [True, False]]
+      , [SetUnderlining u | u <- [SingleUnderline, DoubleUnderline, NoUnderline]]
+      , [SetBlinkSpeed s | s <- [SlowBlink, RapidBlink, NoBlink]]
+      , [SetVisible v | v <- [True, False]]
+      , [SetSwapForegroundBackground s | s <- [True, False]]
+      ]
+
+
+bfs :: forall a b. (Ord a, Show b) => a -> (a -> Bool) -> (a -> [(a, b)]) -> Maybe [b]
+bfs start done step = go Map.empty (Seq.singleton (start, Nothing))
+  where
+    go :: Map a (Maybe (a, b)) -> Seq (a, Maybe (a, b)) -> Maybe [b]
+    go _ Seq.Empty = Nothing
+    go finished ((state, lineage) :<| fringe)
+      | done state = Just $ reverse $ buildPath finished' state
+      | Map.member state finished = go finished fringe
+      | otherwise = go finished' fringe'
+      where
+        finished' = Map.insert state lineage finished
+        fringe' :: Seq (a, Maybe (a, b))
+        fringe' = fringe >< Seq.fromList [(state', Just (state, edge)) | (state', edge) <- step state] -- (zip (step state) (repeat $ Just state))
+
+    buildPath :: Map a (Maybe (a, b)) -> a -> [b]
+    buildPath parents end = case Map.lookup end parents of
+      Nothing -> error "corrupted path"
+      Just Nothing -> []
+      Just (Just (parent, edge)) -> edge : buildPath parents parent
+
+
+codePlan :: SGRState -> SGRState -> [SGR]
+codePlan source target = case bfs source (== target) step of
+  Just codes -> codes
+  Nothing -> error $ "No path between " ++ show source ++ " and " ++ show target
+  where
+    step :: SGRState -> [(SGRState, SGR)]
+    step state = [(applySGR code state, code) | code <- allCodes colors]
+
+    colors :: [ANSIColor]
+    colors = catMaybes [ foreground source, foreground target
+                       , background source, background target
+                       ]
+
+
+compress :: SGRState -> [SGR] -> [SGR]
+compress start codes = codePlan start end
+  where
+    end = foldl' (flip applySGR) start codes
